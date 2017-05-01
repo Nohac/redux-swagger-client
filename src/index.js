befd6c86e29b9277e2445a8a425bcc428ea44b93
Artur Dudnik <adudnik@gmail.com>
2017-05-02 00:16:28 +0300

    extend syntax (#3)
    * extend syntax
    
    * first fix
    
    * first fix action
    
    * cosmetic changes
    
    * finally, tested
    
    * remove console log

--- src/index.js
+++ src/index.js
@@ -1,38 +1,59 @@
-import Swagger from 'swagger-client'
+import Swagger from 'swagger-client';
 
-export default (opts) => store => next => {
-  var waitQueue = []
-  var ready = false
+export default function swaggerMiddleware(opts) {
+  const waitQueue = [];
+  let client;
+  new Swagger(opts)
+    .then(
+      result => {
+        client = result;
+        //console.log('swaggerMiddleware:inited', result, client);
 
-  var callApi = (swagger) => {
-    if (typeof swagger === 'function')
-      swagger(client)
-    else
-      console.error("Swagger api call is not a function")
-  }
+        while (waitQueue.length) {
+          const a = waitQueue.shift();
+          a(client.apis);
+        }
+      },
+      err => console.error('swaggerMiddleware:inited2', err)
+    ).catch(err => console.error('swaggerMiddleware:inited1', err));
 
-  var client = new Swagger({
-    ...opts,
-    success: () => {
-      ready = true
-      while (waitQueue.length) {
-        var action = waitQueue.shift()
-        callApi(action.swagger)
-        next(action)
-      }
+  return ({ dispatch, getState }) => next => action => {
+    if (typeof action === 'function') {
+      return action(dispatch, getState);
     }
-  });
-  return action => {
-    if (action.swagger) {
-      // Add async api calls to queue if not ready
-      if (ready == false)
-        waitQueue.push(action)
-      else {
-        // Call payload and pass the swagger client
-        callApi(action.swagger)
-        return next(action)
-      }
-    } else 
-      return next(action)
+
+    if (!action.swagger) {
+      return next(action);
+    }
+    const { swagger, types, ...rest } = action;
+    const [REQUEST, SUCCESS, FAILURE] = types;
+    const callApi = (c, sw) => {
+      // console.log('swaggerMiddleware:callApi', c, sw, action);
+      return typeof sw === 'function'
+        ? sw(c)
+          .then(
+            (result) => next({ ...rest, result, type: SUCCESS }),
+            (error) => next({ ...rest, error, type: FAILURE })
+          ).catch(error => {
+            console && console.error && console.error('MIDDLEWARE ERROR:', error);
+            next({ ...rest, error, type: FAILURE });
+          })
+        : console.error('Swagger api call is not a function')
+    };
+    // console.log('swaggerMiddleware: got it!', client, waitQueue);
+
+    // Add async api calls to queue if not ready
+    if (!client) {
+      waitQueue.push((c) => {
+        next({ ...rest, type: REQUEST });
+        callApi(c, swagger);
+      });
+    } else {
+      // Call payload and pass the swagger client
+      next({ ...rest, type: REQUEST });
+      return callApi(client.apis, swagger);
+    }
+
+    return undefined;
   }
 }
